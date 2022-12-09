183fcfc86eed281f82b3fcc09f0ae0a7fe6367c8
Jack Laverty <jack.laverty@haltech.com.au>
2022-12-10 02:33:18 +1100

    Tidying up - mainly removing whitespace from the JSON values

--- src/records/navaid.py
+++ src/records/navaid.py
@@ -55,9 +55,12 @@ class Navaid:
             self.record["Record Type"] = 'Standard'
         elif data == 'T':
             self.record["Record Type"] = 'Tailored'
+        else:
+            print("Invalid Record")
+            exit()
     
     def parse_cycle_date(self, data):
-        self.record["Navaid Class"] = {
+        self.record["Cycle Data"] = {
             "Revision Year": int("20" + data[:2]),
             "Release Cycle": int(data[2:]),
         }
@@ -73,43 +76,63 @@ class Navaid:
         x = len(data)
         self.record["DME Longitude"] = ' '.join([data[1:x-6], data[x-6:x-4], data[x-4:x-2], data[x-2:x], data[0]])
 
+    def parse_vor_lat(self, data):
+        if data.strip() == '':
+            self.record["VOR Latitude"] = '<Blank>'
+        else:
+            x = len(data)
+            self.record["VOR Latitude"] = ' '.join([data[1:x-6], data[x-6:x-4], data[x-4:x-2], data[x-2:x], data[0]])
+
+    def parse_vor_long(self, data):
+        if data.strip() == '':
+            self.record["VOR Longitude"] = '<Blank>'
+        else:
+            x = len(data)
+            self.record["VOR Longitude"] = ' '.join([data[1:x-6], data[x-6:x-4], data[x-4:x-2], data[x-2:x], data[0]])
+    
+    def parse_freq_protection(self, data):
+        self.record["Frequency Protection"] = data.strip() if data.strip() != '' else '<Blank>'
+
+    def parse_ils_dme_bias(self, data):
+        self.record["ILS/DME Bias"] = data.strip() if data.strip() != '' else '<Blank>'
+
+    def parse_station_declination(self, data):
+        self.record["Station Declination"] = data.strip() if data.strip() != '' else '<Blank>'
+
     def parse_record(self, r):
         self.parse_record_type(r[0])
         self.parse_cycle_date(r[128:132])
+        self.record["Section Code"], self.record["Subsection Code"] = sections.parse_section(r)
         if self.parse_continuation(r[21]) < 2:
             self.record["Customer / Area Code"]    = r[1:4]
-            self.record["Section Code"]            = r[4]
-            self.record["Subsection Code"]         = r[5]
             self.record["Airport ICAO Identifier"] = r[6:10]
             self.record["ICAO Code"]               = r[10:12]
             self.record["VOR Identifier"]          = r[13:17]
             self.record["ICAO Code"]               = r[19:21]
             self.parse_freq(r[22:27])
             self.parse_class(r[27:32])
-            self.record["VOR Latitude"]            = r[32:41]
-            self.record["VOR Longitude"]           = r[41:51]
+            self.parse_vor_lat(r[32:41])
+            self.parse_vor_long(r[41:51])
             self.record["DME Ident"]               = r[51:55]
             self.parse_dme_lat(r[55:64])
             self.parse_dme_long(r[64:74])
-            self.record["Station Declination"]     = r[74:79]
+            self.parse_station_declination(r[74:79])
             self.parse_dme_elevation(r[79:84])
             self.record["Figure of Merit"]         = r[84]
-            self.record["ILS/DME Bias"]            = r[85:87]
-            self.record["Frequency Protection"]    = r[87:90]
+            self.parse_ils_dme_bias(r[85:87])
+            self.parse_freq_protection(r[87:90])
             self.record["Datum Code"]              = r[90:93]
-            self.record["VOR Name"]                = r[93:123]
+            self.record["VOR Name"]                = r[93:123].strip()
             self.record["File Record No"]         = r[123:128]
         else:
             self.record["Customer / Area Code"]    = r[1:4]
-            self.record["Section Code"]            = r[4]
-            self.record["Subsection Code"]         = r[5]
             self.record["Airport ICAO Identifier"] = r[6:10]
             self.record["ICAO Code"]               = r[10:12]
             self.record["VOR Identifier"]          = r[13:17]
             self.record["ICAO Code"]               = r[19:21]
             self.record["Application Type"]        = r[22]
             self.record["Notes"]                   = r[23:92]
-            self.record["Reserved (Expansion)"]    = r[92:123]
+            self.record["Reserved (Expansion)"]    = r[92:123] if r[92:123].strip() != '' else '<Blank>' 
             self.record["File Record No"]         = r[123:128]
 
     def dump(self):
