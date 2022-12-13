b72c6565b83c18c05f4c9dc9416c189a23d6e584
Jack Laverty <jack.laverty@haltech.com.au>
2022-12-13 19:53:06 +1100

    adding decoder.py and moving search to .config * it should be a CLI thing, but we'll get there

--- src/main.py
+++ src/main.py
@@ -5,27 +5,42 @@ import records.airport as airport
 import sections
 import json
 
+class Config():
+    input = ''
+    search = ''
+    def init():
+        return
+
 if __name__ == "__main__":
-    f = open("../.config")
-    conf = f.readline().split()
+
+    # reading .config file
+    config = Config()
+    with open("../.config") as f:
+        for line in f.readlines():
+            line = line.strip().split()
+            if line[0] == 'FILE':
+                config.input = line[1]
+            elif line[0] == 'SEARCH':
+                config.search = line[1]
     f.close()
-    if conf[0] == 'FILE':
-        f = open(conf[1])
+
+    if config.input != '':
+        f = open(config.input)
     else:
-        print("invalid .config")
+        print("invalid .config: no input file")
         exit()
 
     dir = os.path.join("..", "output")
     if not os.path.exists(dir):
         os.mkdir(dir)
 
-    w = open(dir + "\YPTN.json", 'w')
+    w = open(dir + "\\" + config.search + ".json", 'w')
     for line in f.readlines():
         
         if line.startswith('S' or 'T') == False:
             continue
         
-        if 'YPTN' not in line:
+        if config.search not in line:
             continue
 
         section = sections.Section()
