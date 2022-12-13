794357b41b16851a68e80c043cc69953d147441e
Jack Laverty <jack.laverty@haltech.com.au>
2022-12-14 00:53:46 +1100

    getting search term from command line

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@ import sections
 import json
 
 class Config():
-    input = ''
-    search = ''
+    input = None
+    search = None
     def init():
         return
 
@@ -20,16 +20,18 @@ if __name__ == "__main__":
             line = line.strip().split()
             if line[0] == 'FILE':
                 config.input = line[1]
-            elif line[0] == 'SEARCH':
-                config.search = line[1]
     f.close()
 
     # try to open the input file
-    if config.input != '':
+    if config.input != None:
         f = open(config.input)
     else:
         print("invalid .config: no input file")
         exit()
+
+    # read search term
+    if len(sys.argv) > 1:
+        config.search = sys.argv[1] 
 
     # create an output folder
     dir = os.path.join("..", "output")
@@ -37,17 +39,20 @@ if __name__ == "__main__":
         os.mkdir(dir)
 
     # create an output file
-    w = open(dir + "\\" + config.search + ".json", 'w')
+    out_file = "output"
+    if config.search != None:
+        out_file += ("_" + config.search)
+    w = open(dir + "\\" + out_file + ".json", 'w')
 
     # read the records
     for line in f.readlines():
         if line.startswith('S' or 'T') == False:
             continue
-        if config.search not in line:
+        if config.search not in line and config.search != None:
             continue
         section = sections.Section()
         section.read(line)
-        print("Record Type:", section.decode())
+        # print("Record Type:", section.decode())
         if section.is_navaid():
             nr = nav.Navaid()
             nr.read(line, section)
