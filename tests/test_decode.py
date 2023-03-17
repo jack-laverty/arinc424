3f1a3915e09cd95cf70112dc09e052d81beec5e7
Jack Laverty <jacktlaverty@gmail.com>
2023-03-18 10:41:10 +1100

    adding the tests back so you can see the decoder WIP broke the build

--- tests/test_decode.py
+++ tests/test_decode.py
@@ -1,48 +1,48 @@
-# import unittest
-# import arinc424.utils as a424
+import unittest
+import arinc424.utils as a424
 
 
-# class TestDecode(unittest.TestCase):
+class TestDecode(unittest.TestCase):
 
-#     def test_0(self):
-#         a424.read_file_and_dec('./data/ARINC-424-18/airport')
+    def test_0(self):
+        a424.read_file_and_dec('./data/ARINC-424-18/airport')
 
-#     def test_1(self):
-#         a424.read_file_and_dec('./data/ARINC-424-18/airport_communications')
+    def test_1(self):
+        a424.read_file_and_dec('./data/ARINC-424-18/airport_communications')
 
-#     def test_2(self):
-#         a424.read_file_and_dec('./data/ARINC-424-18/cruising_tables')
+    def test_2(self):
+        a424.read_file_and_dec('./data/ARINC-424-18/cruising_tables')
 
-#     def test_3(self):
-#         a424.read_file_and_dec('./data/ARINC-424-18/enroute_airway')
+    def test_3(self):
+        a424.read_file_and_dec('./data/ARINC-424-18/enroute_airway')
 
-#     def test_4(self):
-#         a424.read_file_and_dec('./data/ARINC-424-18/enroute_airway_restricted')
+    def test_4(self):
+        a424.read_file_and_dec('./data/ARINC-424-18/enroute_airway_restricted')
 
-#     def test_5(self):
-#         a424.read_file_and_dec('./data/ARINC-424-18/enroute_holding')
+    def test_5(self):
+        a424.read_file_and_dec('./data/ARINC-424-18/enroute_holding')
 
-#     def test_6(self):
-#         a424.read_file_and_dec('./data/ARINC-424-18/enroute_marker')
+    def test_6(self):
+        a424.read_file_and_dec('./data/ARINC-424-18/enroute_marker')
 
-#     def test_7(self):
-#         a424.read_file_and_dec('./data/ARINC-424-18/enroute_waypoint')
+    def test_7(self):
+        a424.read_file_and_dec('./data/ARINC-424-18/enroute_waypoint')
 
-#     def test_8(self):
-#         a424.read_file_and_dec('./data/ARINC-424-18/fir_uir')
+    def test_8(self):
+        a424.read_file_and_dec('./data/ARINC-424-18/fir_uir')
 
-#     def test_9(self):
-#         a424.read_file_and_dec('./data/ARINC-424-18/gates')
+    def test_9(self):
+        a424.read_file_and_dec('./data/ARINC-424-18/gates')
 
-#     def test_10(self):
-#         a424.read_file_and_dec('./data/ARINC-424-18/heliport')
+    def test_10(self):
+        a424.read_file_and_dec('./data/ARINC-424-18/heliport')
 
-#     def test_11(self):
-#         a424.read_file_and_dec('./data/ARINC-424-18/heliport_communications')
+    def test_11(self):
+        a424.read_file_and_dec('./data/ARINC-424-18/heliport_communications')
 
-#     def test_12(self):
-#         a424.read_file_and_dec('./data/ARINC-424-18/instrument_approaches')
+    def test_12(self):
+        a424.read_file_and_dec('./data/ARINC-424-18/instrument_approaches')
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
